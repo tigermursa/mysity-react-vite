1e60c429d87aeaf2c5d1aee0872adf0348f2a3c1
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-03 18:56:22 +0600

    "updated active links"

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import {
   FaGithubAlt,
   FaGlobe,
@@ -14,98 +14,124 @@ import {
   FaWandSparkles,
   FaWhmcs,
 } from "react-icons/fa6";
+
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <aside className="bg-blue-950 p-4 lg:p-5 col-span-2 h-[100vh] sticky top-0 left-0 overflow-auto">
-      <NavLink>
-        <ul className="flex flex-col text-gray-500 font-bold font-mono ">
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/"}
-          >
-            <FaHouseChimneyWindow /> Home
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/development"}
-          >
-            <FaWhmcs />
-            Development
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2   `}
-            href={"/dashboard/ui"}
-          >
-            <FaWandSparkles />
-            UI/UX
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/learn"}
-          >
-            <FaUserGraduate /> Learning
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/ai"}
-          >
-            <FaGithubAlt />
-            AI
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/github"}
-          >
-            <FaSquareGithub />
-            GitHub
-          </NavLink>
-
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/news"}
-          >
-            <FaGlobe /> News
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2   `}
-            href={"/dashboard/google"}
-          >
-            <FaGoogle />
-            Google
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/resume"}
-          >
-            <FaIdCard />
-            Resume
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/search"}
-          >
-            <FaSistrix /> Search
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2   `}
-            href={"/dashboard/others"}
-          >
-            <FaTag /> Others
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/add"}
-          >
-            <FaPlus /> Add website
-          </NavLink>
-          <NavLink
-            className={`mb-3 hover:text-white flex items-center gap-2  `}
-            href={"/dashboard/show"}
-          >
-            <FaRegEye /> Show
-          </NavLink>
-        </ul>
-      </NavLink>
+      <ul className="flex flex-col text-gray-500 font-bold font-mono ">
+        <NavLink
+          to="/dashboard"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/dashboard") ? "text-white" : ""
+          }`}
+        >
+          <FaHouseChimneyWindow /> Home
+        </NavLink>
+        <NavLink
+          to="/dev"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/dev") ? "text-white" : ""
+          }`}
+        >
+          <FaWhmcs />
+          Development
+        </NavLink>
+        <NavLink
+          to="/ui"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/ui") ? "text-white" : ""
+          }`}
+        >
+          <FaWandSparkles />
+          UI/UX
+        </NavLink>
+        <NavLink
+          to="/learn"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/learn") ? "text-white" : ""
+          }`}
+        >
+          <FaUserGraduate /> Learning
+        </NavLink>
+        <NavLink
+          to="/ai"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/ai") ? "text-white" : ""
+          }`}
+        >
+          <FaGithubAlt />
+          AI
+        </NavLink>
+        <NavLink 
+          to="/git"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/git") ? "text-white" : ""
+          }`}
+        >
+          <FaSquareGithub />
+          GitHub
+        </NavLink>
+        <NavLink
+          to="/news"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/news") ? "text-white" : ""
+          }`}
+        >
+          <FaGlobe /> News
+        </NavLink>
+        <NavLink
+          to="/google"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/google") ? "text-white" : ""
+          }`}
+        >
+          <FaGoogle />
+          Google
+        </NavLink>
+        <NavLink
+          to="/resume"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/resume") ? "text-white" : ""
+          }`}
+        >
+          <FaIdCard />
+          Resume
+        </NavLink>
+        <NavLink
+          to="/search"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/search") ? "text-white" : ""
+          }`}
+        >
+          <FaSistrix /> Search
+        </NavLink>
+        <NavLink
+          to="/others"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/others") ? "text-white" : ""
+          }`}
+        >
+          <FaTag /> Others
+        </NavLink>
+        <NavLink
+          to="/add"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/add") ? "text-white" : ""
+          }`}
+        >
+          <FaPlus /> Add website
+        </NavLink>
+        <NavLink
+          to="/test"
+          className={`mb-3 hover:text-white flex items-center gap-2 ${
+            location.pathname.startsWith("/test") ? "text-white" : ""
+          }`}
+        >
+          <FaRegEye /> Show
+        </NavLink>
+      </ul>
     </aside>
   );
 };
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,19 @@ import App from "../App";
 
 import Dashboard from "../Components/Dashboard";
 import Another from "../Components/Another";
+import Development from "../pages/Development";
+import Ui from "../pages/UI";
+import Learning from "../pages/Learning";
+import Ai from "../pages/AI";
+import GitHub from "../pages/GitHub";
+import News from "../pages/News";
+import Others from "../pages/Others";
+import Resume from "../pages/Resume";
+import Search from "../pages/Search";
+import AddWebsite from "../Components/Dashboard/AddWebsite";
+import UpdateWebsite from "../Components/Dashboard/UpdateWebsite";
+import TestingShow from "../Components/Dashboard/TestingShow";
+import Google from "../pages/Google";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +29,58 @@ const router = createBrowserRouter([
       {
         path: "/another",
         element: <Another />,
+      },
+      {
+        path: "/dev",
+        element: <Development />,
+      },
+      {
+        path: "/ui",
+        element: <Ui />,
+      },
+      {
+        path: "/learn",
+        element: <Learning />,
+      },
+      {
+        path: "/ai",
+        element: <Ai />,
+      },
+      {
+        path: "/git",
+        element: <GitHub />,
+      },
+      {
+        path: "/news",
+        element: <News />,
+      },
+      {
+        path: "/others",
+        element: <Others />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/add",
+        element: <AddWebsite />,
+      },
+      {
+        path: "/update",
+        element: <UpdateWebsite />,
+      },
+      {
+        path: "/test",
+        element: <TestingShow />,
+      },
+      {
+        path: "/google",
+        element: <Google />,
       },
     ],
   },
